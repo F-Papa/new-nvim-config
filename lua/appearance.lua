ebb43e14bc39354a54c487429c534f2f184e424d
F-Papa <francopapa21@gmail.com>
2025-05-21 14:47:15 -0400

    feat(appearance): change line no. colors

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -15,3 +15,6 @@ vim.opt.expandtab = true
 
 
 vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("highlight LineNr ctermfg=12 guifg=#e6e6e6")
+vim.cmd("highlight LineNrAbove ctermfg=12 guifg=#a6a6a6")
+vim.cmd("highlight LineNrBelow ctermfg=12 guifg=#a6a6a6")
