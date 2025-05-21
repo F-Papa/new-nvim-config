fe469913c3228327f36d15e7fb23949297df42f3
F-Papa <francopapa21@gmail.com>
2025-05-21 15:33:07 -0400

    feat(keymaps): add :copen and window nav keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,2 +1,11 @@
+--Quickfix
 vim.keymap.set("n", "<M-j>", ":cnext<CR>")
 vim.keymap.set("n", "<M-k>", ":cprev<CR>")
+vim.keymap.set("n", "<M-o>", ":copen<CR>")
+
+--Windows
+vim.keymap.set("n", "<C-h>", "<C-W>h")
+vim.keymap.set("n", "<C-j>", "<C-W>j")
+vim.keymap.set("n", "<C-k>", "<C-W>k")
+vim.keymap.set("n", "<C-l>", "<C-W>l")
+vim.keymap.set("n", "<C-q>", "<C-W>q")
