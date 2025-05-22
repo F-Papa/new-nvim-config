b2a0d3d711898c68fbe382c849da69e46a2ebbc1
F-Papa <francopapa21@gmail.com>
2025-05-21 21:19:50 -0400

    feat(appearance): use kanagawa-dragon as theme

--- lua/appearance.lua
+++ lua/appearance.lua
@@ -12,7 +12,4 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-vim.cmd("colorscheme tokyonight-moon")
-vim.cmd("highlight LineNr ctermfg=12 guifg=#e6e6e6")
-vim.cmd("highlight LineNrAbove ctermfg=12 guifg=#a6a6a6")
-vim.cmd("highlight LineNrBelow ctermfg=12 guifg=#a6a6a6")
+vim.cmd("colorscheme kanagawa-dragon")
