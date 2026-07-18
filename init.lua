df48e8ca999684157c55567764f2f35327a38b86
F-Papa <francopapa21@gmail.com>
2026-07-18 19:55:19 -0400

    add WSL clipboard integration

--- init.lua
+++ init.lua
@@ -1,6 +1,24 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+
+  cache_enabled = 0,
+}
+
+vim.opt.clipboard = "unnamedplus"
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
